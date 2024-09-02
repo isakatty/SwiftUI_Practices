2af66b0af10e6ed73721c7e80bbad44a0c098ef2
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-09-02 18:35:19 +0900

    [Setting] 프로젝트 초기 설정

---
+++ SwiftUI_Practices/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Practices
+//
+//  Created by Jisoo Ham on 9/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Practices/SwiftUI_PracticesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticesApp.swift
+//  SwiftUI_Practices
+//
+//  Created by Jisoo Ham on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
